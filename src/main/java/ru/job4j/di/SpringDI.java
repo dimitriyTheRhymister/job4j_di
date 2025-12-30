ad5690e6d5d1b69520bc521a8623d510fe8c03ee
dimitriy <rhymister19@gmail.com>
2025-12-31 00:13:22 +0300

    1. Spring DI. Запуск [#2091]

---
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -0,0 +1,22 @@
+package ru.job4j.di;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SpringDI {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+
+        context.register(Store.class);
+        context.register(ConsoleInput.class);
+        context.register(StartUI.class);
+        context.refresh();
+
+        StartUI ui = context.getBean(StartUI.class);
+
+        ui.add("User #1");
+        ui.addViaConsole();
+        ui.add("User #2");
+        ui.addViaConsole();
+        ui.print();
+    }
+}
